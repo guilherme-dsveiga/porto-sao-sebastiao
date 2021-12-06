56a6ed6422f6d59b58b7c5f31d93fbe8024df0cf
guilherme-dsveiga <guilherme.dsveiga@gmail.com>
2021-12-06 16:32:59 -0300

    Adicionando mapa para teste

---
+++ scripts/map.js
@@ -0,0 +1,156 @@
+
+$(function () {
+    $(".a-grande").on({
+        mouseenter: function () {
+            $(".grande").show()
+        },
+        mouseleave: function () {
+            $(".grande").hide()
+        }
+    })
+    $(".a-alegre").on({
+        mouseenter: function () {
+            $(".alegre").show()
+        },
+        mouseleave: function () {
+            $(".alegre").hide()
+        }
+    })
+    $(".a-imbituba").on({
+        mouseenter: function () {
+            $(".imbituba").show()
+        },
+        mouseleave: function () {
+            $(".imbituba").hide()
+        }
+    })
+    $(".a-itajai").on({
+        mouseenter: function () {
+            $(".itajai").show()
+        },
+        mouseleave: function () {
+            $(".itajai").hide()
+        }
+    })
+    $(".a-francisco").on({
+        mouseenter: function () {
+            $(".francisco").show()
+        },
+        mouseleave: function () {
+            $(".francisco").hide()
+        }
+    })
+    $(".a-paranagua").on({
+        mouseenter: function () {
+            $(".paranagua").show()
+        },
+        mouseleave: function () {
+            $(".paranagua").hide()
+        }
+    })
+    $(".a-santos").on({
+        mouseenter: function () {
+            $(".santos").show()
+        },
+        mouseleave: function () {
+            $(".santos").hide()
+        }
+    })
+    $(".a-itaguai").on({
+        mouseenter: function () {
+            $(".itaguai").show()
+        },
+        mouseleave: function () {
+            $(".itaguai").hide()
+        }
+    })
+    $(".a-janeiro").on({
+        mouseenter: function () {
+            $(".janeiro").show()
+        },
+        mouseleave: function () {
+            $(".janeiro").hide()
+        }
+    })
+    $(".a-acu").on({
+        mouseenter: function () {
+            $(".acu").show()
+        },
+        mouseleave: function () {
+            $(".acu").hide()
+        }
+    })
+    $(".a-vitoria").on({
+        mouseenter: function () {
+            $(".vitoria").show()
+        },
+        mouseleave: function () {
+            $(".vitoria").hide()
+        }
+    })
+    $(".a-salvador").on({
+        mouseenter: function () {
+            $(".salvador").show()
+        },
+        mouseleave: function () {
+            $(".salvador").hide()
+        }
+    })
+    $(".a-fortaleza").on({
+        mouseenter: function () {
+            $(".fortaleza").show()
+        },
+        mouseleave: function () {
+            $(".fortaleza").hide()
+        }
+    })
+    $(".a-luis").on({
+        mouseenter: function () {
+            $(".luis").show()
+        },
+        mouseleave: function () {
+            $(".luis").hide()
+        }
+    })
+    $(".a-belem").on({
+        mouseenter: function () {
+            $(".belem").show()
+        },
+        mouseleave: function () {
+            $(".belem").hide()
+        }
+    })
+    $(".a-conde").on({
+        mouseenter: function () {
+            $(".conde").show()
+        },
+        mouseleave: function () {
+            $(".conde").hide()
+        }
+    })
+    $(".a-santarem").on({
+        mouseenter: function () {
+            $(".santarem").show()
+        },
+        mouseleave: function () {
+            $(".santarem").hide()
+        }
+    })
+    $(".a-juriti").on({
+        mouseenter: function () {
+            $(".juriti").show()
+        },
+        mouseleave: function () {
+            $(".juriti").hide()
+        }
+    })
+    $(".a-macapa").on({
+        mouseenter: function () {
+            $(".macapa").show()
+        },
+        mouseleave: function () {
+            $(".macapa").hide()
+        }
+    })
+
+});
